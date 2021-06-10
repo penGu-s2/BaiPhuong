f10c4dc0c733a359472b25dd3991be6e73ee6e82
s2Shit91 <thangnd@tandan.com.vn>
2021-06-11 00:19:05 +0700

    update

--- BaiTap.c
+++ BaiTap.c
@@ -271,3 +271,210 @@ int main()
 	tinhtongdandau(a,m);
 	return 0;
 }
+//Bài 6
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdbool.h>
+#include <math.h>
+ bool kiemtranto(int n){
+    	int i;
+    	if (n < 2)
+        return false;
+    	for (i = 2; i <= sqrt(n); i++)
+   		{
+      	  if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+	}
+void kiemtraSNT(int a[],int m){
+	int i;
+		printf("\nday so nguyen to  :");
+		for(i=0;i<m;i++){
+			if (kiemtranto(a[i]))
+        {
+            printf(" %d",a[i]);
+        }
+			}
+	}
+void kiemtrachinhphuong(int a[],int m){
+	int i;
+	int x;
+	int flag=0;
+		printf("\nday so chinh phuong  :");
+	for(i=0;i<m;i++){
+      x=trunc(sqrt(a[i]));
+      if(x*x==a[i])
+      	printf(" %d",a[i]);
+    }
+}
+void kiemtratong(int a[],int m){
+	int i;
+	int tongchan=0;
+	int tongle=0;
+	int flag=0;
+	for(i=0;i<m;i++){
+        if(a[i]%2==0)
+          {
+          	tongchan+=a[i];
+		  }else{
+		  	tongle+=a[i];
+		  }
+    }
+    
+        printf("\nTong chan la :%d",tongchan);
+		printf("\nTong le la :%d",tongle);
+}
+void kiemtratongamduong(int a[],int m){
+	int i;
+	int tongam=0;
+	int tongduong=0;
+	int flag=0;
+	for(i=0;i<m;i++){
+        if(a[i]<=0)
+          {
+          	tongam+=a[i];
+		  }else{
+		  	tongduong+=a[i];
+		  }
+    }
+    
+        printf("\nTong am la :%d",tongam);
+		printf("\nTong duong la :%d",tongduong);
+}
+bool checkSohoanHao(int n){
+	int i;
+    int sum = 0;
+    for(i=1;i<=n/2;i++){
+        if(n%i==0) 
+            sum+=i;
+    }
+    if(sum==n) return true;
+    return false;
+}
+void Sohoanhao(int a[], int n){
+	int i;
+	printf("\nday so hoan hao  :");
+	for(i=0;i<n;i++){
+        if(checkSohoanHao(a[i]))
+          {
+          	printf(" %d",a[i]);
+		  }
+    }
+	}
+int main()
+{
+  int m;
+  int i,j,k;
+  int a[100];
+  int dem=0;
+  printf("Nhap m = ");
+  scanf("%d", &m);
+  for(i=0; i<m ;i++){
+  	 printf("\nNhap phan tu a[%d]  = ", i);
+  	scanf("%d",&a[i]);
+	  }
+
+	kiemtraSNT(a,m);
+	kiemtrachinhphuong(a,m);
+	kiemtratong(a,m);
+	kiemtratongamduong(a,m);
+	Sohoanhao(a,m);
+	return 0;
+}
+//bài 7 
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdbool.h>
+#include <math.h>
+int main()
+{
+  int m;
+  int i,j,k;
+  int a[100];
+  int dem=0;
+  printf("Nhap m = ");
+  scanf("%d", &m);
+  for(i=0; i<m ;i++){
+  	 printf("\nNhap phan tu a[%d]  = ", i);
+  	scanf("%d",&a[i]);
+	  }
+	printf("\nday so vua nhap  :");
+	for(i=0;i<m;i++){
+          	printf(" %d",a[i]);
+    }
+	return 0;
+}
+//Bài 8
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <stdbool.h>
+#include <math.h>
+#define max 100
+void Tong(int a[max][max], int b[max][max], int c[max][max],int m, int n){
+	int i,j;
+   for(i = 0; i<m ; i++){
+   for(j = 0; j<n ; j++){
+    c[i][j] = a[i][j]+b[i][j];
+    }
+    }
+    for(i = 0; i<m ; i++)
+   {
+      printf("\n");
+      for(j = 0; j<n ; j++)
+         printf("%d\t",c[i][j]);
+   }
+}
+void Tich(int a[max][max], int b[max][max], int c[max][max],int m, int n)
+{
+	int i,k,j;
+   for(i = 0; i<m ; i++){
+   for(k = 0; k<n ; k++)
+   {
+      c[i][k] = 0;
+      for(j = 0; j<n ; j++)
+         c[i][k] = c[i][k] + a[i][j]*b[j][k];
+   }
+   }
+    for(i = 0; i<m ; i++)
+   {
+      printf("\n");
+      for(j = 0; j<n ; j++)
+         printf("%d\t",c[i][j]);
+   }
+}
+	
+int main()
+{
+  int m,n;
+  int i,j,k;
+  int a[max][max];
+  int b[max][max];
+	int c[max][max];
+  int dem=0;
+  printf("Nhap ma tran m x n = ");
+  scanf("%d%d", &m,&n);
+  for(i=0; i<m ;i++){
+   for(j=0; j<n ;j++){
+  	 printf("\nNhap phan tu a[%d][%d]  = ", i,j);
+  	scanf("%d", &a[i][j]);
+	  }}
+	   for(i=0; i<m ;i++){
+	    for(j=0; j<n ;j++){
+  	 printf("\nNhap phan tu b[%d][%d]  = ", i,j);
+  	scanf("%d", &b[i][j]);
+	  }}
+
+    printf("\nTong 2 ma tran la");
+    Tong(a,b,c,m,n);
+    
+    printf("\nTich 2 ma tran la");
+    
+    Tich(a,b,c,m,n);
+	return 0;
+}
